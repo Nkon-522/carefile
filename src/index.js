631c618ae71cd42fa686f959d2db4ecf29a6459a
Nkon-522 <nkon.cs@gmail.com>
2023-07-17 15:55:20 -0500

    Update - Basic server in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!a")
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+app.listen(3000)
+console.log("Server is listening on port", 3000);
